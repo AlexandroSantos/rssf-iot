9d6ef1ebcde17c57a88b7c44b8f5d7ba299be0d2
AlexandroSantos <alex061275@gmail.com>
2019-04-09 18:58:19 -0300

    desafio

--- praticas/pratica_02/sensor-timer-uart.c
+++ praticas/pratica_02/sensor-timer-uart.c
@@ -9,16 +9,20 @@
 #define BUF_SIZE 8
 static int buffer[BUF_SIZE];
 static int buf_c = 0;
+static int min_t = 100;
+static int max_t = 0;
 
 static struct etimer et_sensor;
 static struct etimer et_uart;
+static struct etimer et_uart_minmax;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(sensor_process, "Sensor process");
 PROCESS(uart_process, "Serial process");
+PROCESS(uart_process_minmax, "Serial process min max");
 
 /*---------------------------------------------------------------------------*/
-AUTOSTART_PROCESSES(&sensor_process,&uart_process);
+AUTOSTART_PROCESSES(&sensor_process,&uart_process,&uart_process_minmax);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(sensor_process, ev, data)
 {
@@ -30,8 +34,8 @@ PROCESS_THREAD(sensor_process, ev, data)
 
   while(1) {
     PROCESS_WAIT_EVENT();
-    //if(ev == PROCESS_EVENT_TIMER)  // se passou um segundo
-    if(ev == 142)
+    if(ev == PROCESS_EVENT_TIMER)  // se passou um segundo
+    //if(ev == 142)
     {
         etimer_reset(&et_sensor); // reinicia timer
 
@@ -41,7 +45,8 @@ PROCESS_THREAD(sensor_process, ev, data)
 
         buffer[buf_c++]=val;
         if (buf_c>7) buf_c=0;
-
+        if (min_t>val) min_t=val;
+        if (max_t<val) max_t=val;
         printf("Leu %d\n", val);
     }
 
@@ -72,6 +77,30 @@ PROCESS_THREAD(uart_process, ev, data)
         }
         avg = avg/BUF_SIZE;
         printf("Temperatura media: %d\n", avg);
+    }
+  }
+
+  PROCESS_END();
+  }
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(uart_process_minmax, ev, data)
+{
+  PROCESS_BEGIN();
+
+  /* Insira seu código aqui */
+  etimer_set(&et_uart_minmax, 20*CLOCK_SECOND); // a cada 20 segundos
+
+
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    if(ev == PROCESS_EVENT_TIMER)  // se passaram 20 segundos
+    {
+        /* Insira seu código aqui */
+        etimer_reset(&et_uart_minmax); // reinicia timer
+
+        printf("Temperatura maxima: %d  minima: %d\n", max_t, min_t);
+        //printf("Temperatura minima: %d\n", min_t);
     }
   }
 
